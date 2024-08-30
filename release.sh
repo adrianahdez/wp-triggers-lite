59ce565e92846ecf0621e0c1fb081c2007a249b7
Adriana Hernández R <adriana.hernandez@vass.es>
2024-08-30 10:48:26 +0200

    feat: create script to generate a release zip

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+RELEASE_DIR="releases"
+# Get the 5-character commit hash
+COMMIT_HASH=$(git rev-parse --short=5 HEAD)
+# Define the zip file name
+ZIP_NAME="release-${COMMIT_HASH}.zip"
+
+# Define the release directory and create it if it doesn't exist
+mkdir -p "$RELEASE_DIR"
+
+# Create the zip file, excluding the specified files and directories
+zip -r "$RELEASE_DIR/$ZIP_NAME" . -x "*.git*" -x ".github/*" -x "readme.md" -x "release.sh" -x ".gitignore" -x "$RELEASE_DIR/*"
